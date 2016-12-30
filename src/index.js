10ab0ce27bc165564748ee47e3258e33b7890367
mhaagens <martin.haagensli@gmail.com>
2016-12-30 08:33:03 +0100

    Change class name to LazyRoute

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 
-export default class AsyncRoute extends Component {
+export default class LazyRoute extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
